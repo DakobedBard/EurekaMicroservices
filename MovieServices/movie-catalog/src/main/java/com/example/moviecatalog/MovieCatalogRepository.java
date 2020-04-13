6d62e2c4e9e34495030278eed7af7ea278345636
Mathias Darr <mddarr@gmail.com>
2020-04-12 20:55:12 -0700

    We are annotating a method getRestTemplate()  in the main class of the MoviecatalogApplication w/ the bean annotation.  This method will create a singleton instance of the RestTemplate class which will be Autowired using teh @Autowired annotation

--- MovieServices/movie-catalog/src/main/java/com/example/moviecatalog/MovieCatalogRepository.java
+++ MovieServices/movie-catalog/src/main/java/com/example/moviecatalog/MovieCatalogRepository.java
@@ -1,6 +1,7 @@
 package com.example.moviecatalog;
 
+import com.example.moviecatalog.model.models.CatalogItem;
 import org.springframework.data.repository.CrudRepository;
 
-public interface MovieCatalogRepository extends CrudRepository<MovieCatalog, Integer> {
+public interface MovieCatalogRepository extends CrudRepository<CatalogItem, Integer> {
 }
--- MovieServices/movie-catalog/src/main/java/com/example/moviecatalog/resources/MovieCatalogResource.java
+++ MovieServices/movie-catalog/src/main/java/com/example/moviecatalog/resources/MovieCatalogResource.java
@@ -1,20 +1,57 @@
 package com.example.moviecatalog.resources;
 
 import com.example.moviecatalog.model.models.CatalogItem;
+import com.example.moviecatalog.model.models.Movie;
+import com.example.moviecatalog.model.models.Rating;
+import com.example.moviecatalog.model.models.UserRating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
+        @Autowired
+        private RestTemplate restTemplate;
+
+
         @RequestMapping("/{userId}")
         public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
-            return Collections.singletonList(
-                    new CatalogItem("The Sixth Sense", "Scary Movie",5)
+            RestTemplate restTemplate = new RestTemplate();
+            // Get all rate movie IDs
+
+            // For each movie ID, call movie info servers
+            List<Rating> ratings = Arrays.asList(
+                    new Rating("The ring",2),
+                    new Rating("Bravheart",4)
             );
+            return ratings.stream().map(rating -> {
+                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+                    return new CatalogItem(movie.getName(), "Desc", rating.getRating());
+                    }).collect(Collectors.toList());
+
+//            ratings.stream().map(rating->{
+//                new CatalogItem("The Sixth Sense", "Scary Movie",5)
+//            ).collect(Collectors.toList());
+//
+//            UserRating userRating = restTemplate.getForObject("http://ratings-data-service/ratingsdata/user/" + userId, UserRating.class);
+//            return userRating.getRatings().stream()
+//                        .map(rating -> {
+//                            Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
+//                            return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
+//                        })
+//                        .collect(Collectors.toList());
+
+//
+//            return Collections.singletonList(
+//                    new CatalogItem("The Sixth Sense", "Scary Movie",5)
+//            );
         }
     }
